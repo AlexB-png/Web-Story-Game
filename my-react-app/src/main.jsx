707060b86da89223427e53534af4bb487f9ae4b6
goldensyrup-png <alexander.brankin.466@accesscreative.ac.uk>
2025-09-24 20:28:44 +0100

    More Stuff

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,15 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-function Button(Text) {
-  const MyFunc = () => {
-    alert('Hello World')
-  };
-  return (
-    <button onClick={MyFunc} disabled={false}>{Text.text}</button>
-  );
-}
-
 createRoot(document.getElementById('root')).render(
-  <Button text='Hello Button'/>
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
