e90728ce488084324b12531057399e2f777f83e0
goldensyrup-png <alexander.brankin.466@accesscreative.ac.uk>
2025-10-01 12:33:25 +0100

    pep8

--- my-react-app/PythonAPI/Functions.py
+++ my-react-app/PythonAPI/Functions.py
@@ -4,27 +4,32 @@ import sqlite3
 def Login(username, password):
     connection = sqlite3.connect("Data.sql")
     cursor = connection.cursor()
-    data = cursor.execute("SELECT user,pass FROM data WHERE user=? AND pass=?", (username, password))
+    data = cursor.execute("SELECT user,pass FROM data WHERE user=? AND pass=?",
+                          (username, password))
     if data.fetchone():
         return True
     else:
         return False
 
+
 def Create(username, password):
     connection = sqlite3.connect('Data.sql')
     cursor = connection.cursor()
-    if Check(username):
-        cursor.execute("INSERT INTO data (user , pass) VALUES (? , ?)", (username, password))
+    if Check(username):  # User doesn't exist = True #
+        cursor.execute("INSERT INTO data (user , pass) VALUES (? , ?)",
+                       (username, password))
         connection.commit()
         return True
     else:
         return False
 
+
 def Check(username):
     connection = sqlite3.connect("Data.sql")
     cursor = connection.cursor()
-    data = cursor.execute("SELECT user FROM data WHERE user=?",(username,))
-    return data.fetchone() is None  ## If user exists return False ##
+    data = cursor.execute("SELECT user FROM data WHERE user=?", (username,))
+    return data.fetchone() is None  # If user exists return False #
+
 
 if __name__ == '__main__':
     # THIS IS STRICTLY FOR TESTING PURPOSES #
@@ -35,4 +40,3 @@ if __name__ == '__main__':
     ##
 
     print(Create(user, password))
-
